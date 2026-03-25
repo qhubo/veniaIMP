7ce66b45db6528db34f7ed26d585e885c5654547
abraham Araujo <abrantar@gmail.com>
2026-03-25 16:35:50 -0600

    ajuste estado cuenta

--- apps/venia/modules/estado_cuenta/actions/actions.class.php
+++ apps/venia/modules/estado_cuenta/actions/actions.class.php
@@ -88,7 +88,7 @@ class estado_cuentaActions extends sfActions {
 
     // ================= SALDO INICIAL =================
     $sumatorias = OperacionQuery::create()
-        ->where("Operacion.Fecha < '" . $fechaInicial . " 01:01:01'")
+        ->where("Operacion.Fecha < '" . $fechaInicial . " 00:00:00'")
         ->withColumn('sum(Operacion.ValorTotal)', 'TotalTotal')
         ->filterByEstatus('Anulado', Criteria::NOT_EQUAL)
         ->filterByClienteId($clientev)
