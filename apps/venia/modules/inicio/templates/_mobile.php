e0e08f3eb382617296a1641481112219c63f7a8a
INTEGRADOR_SAP <INTEGRADOR_SAP@DESKTOP-S74JAIB>
2026-09-20 20:36:53 -0600

    tabla

--- apps/venia/modules/inicio/templates/_mobile.php
+++ apps/venia/modules/inicio/templates/_mobile.php
@@ -1,4 +1,5 @@
-        <div id="kt_header_mobile" class="kt-header-mobile  kt-header-mobile--fixed " >
+<?php if (sfContext::getInstance()->getUser()->getAttribute("imagen", null, 'seguridad')) {  ?>        
+<div id="kt_header_mobile" class="kt-header-mobile  kt-header-mobile--fixed " >
             <div class="kt-header-mobile__logo">
                 <a href="#">
                                      <img height="35px" alt="Logo" src="<?php echo sfContext::getInstance()->getUser()->getAttribute("imagen", null, 'seguridad'); ?>"/>
@@ -10,3 +11,4 @@
                 <button class="kt-header-mobile__toolbar-topbar-toggler" id="kt_header_mobile_topbar_toggler"><i class="flaticon-more-1"></i></button>
             </div>
         </div>
+<?php } ?>
